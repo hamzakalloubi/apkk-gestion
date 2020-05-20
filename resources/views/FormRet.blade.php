9d0453d17babc1c8cc0f031fa571a25786d1356f
Ayoub Bentrika <abentrika1998@gmail.com>
2020-05-20 12:52:48 +0200

    template

---
+++ resources/views/FormRet.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="{{mix('/css/app.css')}}">
+<link rel="stylesheet" href="{{mix('/css/theme.css')}}">
+
+    <title>Gestion</title>
+</head>
+<body>
+  @extends('index')
+   @section('content')
+       
+
+ <!-- MAIN CONTENT-->
+ <div class="main-content">
+  <div class="section__content section__content--p30">
+      <div  class="container-fluid">
+          <div  class="row">
+              <div class="col-lg-6">
+                  <div class="card">
+                      <div class="card-header">Formulaire</div>
+                      <div class="card-body">
+                          <div class="card-title">
+                              <h3 class="text-center title-2">Demande de retrait</h3>
+                          </div>
+                          <hr>
+                          <form   action="" method="post" novalidate="novalidate">
+
+                            <div class="row">
+                              <div class="col-6">
+                                 <div class="form-group has-success">
+                                     <label >Nom
+                                     <input style="" type="text" class="form-control" id="" placeholder="Nom d'etudiant">
+                                     <small id="" class="form-text text-muted"></small>
+                                  </div>
+                              </div>
+                              <div class="col-6">
+                              <div class="form-group">
+                                <label >Prénom
+                                  <input style="" type="text" class="form-control" id="" placeholder="Prénom d'etudiant">
+                                  
+                                  <small id="" class="form-text text-muted"></small>
+                                </div>
+                              </div>
+                            </div>
+
+
+                                   <div class="row">
+                                     <div class="col-6">
+                                        <div class="form-group has-success">
+                                            <label >CNE
+                                            <input style="" type="text" class="form-control" id="" placeholder="CNE d'etudiant">
+                                            <small id="" class="form-text text-muted"></small>
+                                         </div>
+                                     </div>
+                                     <div class="col-6">
+                                          <div class="form-group">
+                                         <label >Apogée
+                                        <input style=""type="text" class="form-control" id="" placeholder="apogée d'etudiant">
+                                           <small id="" class="form-text text-muted"></small>
+                                       </div>
+                                </div>
+                                   </div>
+
+                              <div class="row">
+                                  <div class="col-6">
+                                      <div class="form-group">
+                                        <label >Diplome
+                                          <input style="" type="text" class="form-control" id="" placeholder="Diplome d'etudiant">
+                                          
+                                          <small id="" class="form-text text-muted"></small>
+                                      </div>
+                                  </div>
+                                  <div class="col-6">
+                                    <label >Anneé scolarité
+                                      <input style="margin-left: 1px; " type="datetime" class="form-control" id="" placeholder="date">
+                                      <small id="" class="form-text text-muted"></small>
+                                      </div>
+                                  </div>
+                              </div>
+
+
+                              <div class="row">
+                                  <div class="col-6">
+                                    <button id="Retrait temporaire" type="submit" class="btn btn-lg btn-success btn-block">
+                                      <i class="fas fa-stopwatch"></i>&nbsp;
+                                      <span id="payment-button-amount">Retrait temporaire</span>
+                                      <span id="payment-button-sending" style="display:none;">Sending…</span>
+                                  </button>
+                                  </div>
+                                  <div class="col-6">
+                                    <button id="Retrait final" type="submit" class="btn btn-lg btn-dark btn-block">
+                                      <i class="fas fa-ban"></i>&nbsp;
+                                      <span id="payment-button-amount">Retrait final</span>
+                                      <span id="payment-button-sending" style="display:none;">Sending…</span>
+                                  </button>
+                                  </div>
+
+                              </div>
+
+
+                              <div style="margin-top: 2px;">
+                                  <button id="payment-button" type="submit" class="btn btn-lg btn-primary btn-block" >
+                                      <i class="fas fa-paper-plane"></i>&nbsp;
+                                      <span id="payment-button-amount">Envoyer</span>
+                                      <span id="payment-button-sending" style="display:none;">Sending…</span>
+                                  </button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+    
+
+    
+    @endsection
+    
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/FormRet', function () {
+    return view('FormRet');
+});
+
+Route::get('/template', function () {
+    return view('index');
 });
